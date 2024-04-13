3c56c78e8421907cd0d33c9ce11315a3184e57f4
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-04-12 19:43:53 -0700

    importing components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,13 @@ import {
 } from '@apollo/client';
 import Page from './components/Page';
 import { useState } from 'react';
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Page from "./components/Page";
+import { useLocation } from "react-router-dom";
+
+
 
 // temp solution until we decide on how to store the token
 const token = localStorage.getItem('id_token');
@@ -17,7 +24,8 @@ const client = new ApolloClient({
 });
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const currentPage = useLocation().pathname;
 
   return (
     <ApolloProvider client={client}>
