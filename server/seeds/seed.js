5ecb7ebb59d8be7bc4365507715575d7f9cd5f9c
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-04-13 17:32:25 -0700

    seed complete

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,15 +1,24 @@
 const connection = require('../config/connection');
 const { User, Food } = require('../model');
-// const resetDB = require('./resetDB');
 
 const userData = require('../seeds/user.json');
 const foodData = require('../seeds/food.json');
 
 connection.once('open', async () => {
-  await connection.db.listCollections().toArray().map((e) => e.name).forEach((e) => connection.dropCollection(e));
+  try {
+    const collectionExists = await connection.db.listCollections().toArray();
+    
+    if (collectionExists.length) {    
+      // await connection.dropCollection("foods");
+      // await connection.dropCollection("users");
+      await Promise.allSettled((await connection.db.listCollections().toArray()).map((e) => e.name).map((e) => connection.dropCollection(e)));
+    }
 
-  await Food.insertMany(foodData);
-  await User.insertMany(userData);
+    await Food.insertMany(foodData);
+    await User.insertMany(userData);
+  } catch (err) {
+    console.log("Failed");
+  }
 
   console.log('Food & User Data seeded!');
   process.exit(0);
