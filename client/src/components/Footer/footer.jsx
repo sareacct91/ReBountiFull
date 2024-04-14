0e02b7c4bf3c6cca3a4049c6e0724d707d5507a7
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-04-13 19:23:47 -0700

    very basic footer component

--- client/src/components/Footer/footer.jsx
+++ client/src/components/Footer/footer.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Footer() {
-  return <h1>Footer</h1>
+  return (
+  <footer className="bg-orange h-10">
+    <h3 className="w-max font-bold m-auto text-xl pt-1.5">© ReBountiFull 2024</h3>
+  </footer>
+  );
 }
