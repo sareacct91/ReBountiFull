240fb3f4b0154a07b3b00f25de664ff487b9b33e
Sarah Chambers <chamberssarahann@gmail.com>
2024-04-15 19:24:18 -0700

    worked on buttons, and added new landing page folder

--- client/src/components/AboutUS/aboutus.jsx
+++ client/src/components/AboutUS/aboutus.jsx
@@ -3,7 +3,7 @@ import ImpactImage from "../../assets/images/Aboutus_impact.png"
 export default function AboutUs() {
   return(
     <div className="bg-white">
-    <h1>Our Impact</h1>
+    <h1 className="text-black text-4xl pt-10 ml-10">Our Impact</h1>
     <img src={ ImpactImage } alt= "Pie chart depicting allocation of food" />
   </div>)
 }
---
+++ client/src/components/LandingPage/landingpage.jsx
@@ -0,0 +1,7 @@
+export default function LandingPage() {
+  return(
+    <div className="bg-white">
+      <h1 className="text-black text-4xl pt-10 ml-10">How it works</h1>
+    </div>
+  )
+}
