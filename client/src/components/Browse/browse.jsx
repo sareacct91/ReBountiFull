fc16d8a55e6c509d0cad279f0c07c3ca5a2ef2d4
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-04-16 19:32:38 -0700

    display some of the food items

--- client/src/components/Browse/browse.jsx
+++ client/src/components/Browse/browse.jsx
@@ -1,4 +1,25 @@
+import { useQuery } from "@apollo/client";
+import { QUERY_ALL_FOOD } from "../../utils/queries";
+
 
 export default function Browse() {
-  return <h1>Browse</h1>
+    const { loading, data } = useQuery(QUERY_ALL_FOOD);
+
+    if (loading) return <p>Loading...</p>
+    
+    const { getAllFood } = data;
+    console.log("data :",data);
+
+    return (
+      <div>
+        {getAllFood.map((food) => (
+          <div key={food._id}>
+            <p>Name: {food.name}</p>
+            <p>Price: {food.price}</p>
+            <img src={food.image} alt={food.name}></img>
+          </div>
+        ))}
+      </div>
+    );
+
 }
