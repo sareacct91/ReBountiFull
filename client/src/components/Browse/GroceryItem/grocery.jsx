4ab74a9d189c46a3fc6f5e4657280262ed1f261b
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-04-16 20:50:57 -0700

    added buttons to the items

--- client/src/components/Browse/GroceryItem/grocery.jsx
+++ client/src/components/Browse/GroceryItem/grocery.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { pluralize } from "../../../utils/helpers";
 
 export default function GroceryItem(food) {
@@ -6,13 +7,22 @@ export default function GroceryItem(food) {
   return (
     <div className="m-3">
       <div className=" grid w-fit place-items-center text-center">
-        <img className="rounded" alt={name} src={image} />
+        <img
+          className="rounded border-4 border-gray-300 p-2"
+          alt={name}
+          src={image}
+        />
         <div className="mt-2 grid w-fit place-items-center">
-          <p className="font-medium">{name}</p>
+          <p className="font-bold text-black">{name}</p>
           <p className=" text-gray-600">
             {inventory}&nbsp;{pluralize("item", inventory)}&nbsp;in&nbsp;stock
           </p>
-          <span className="text-lg font-bold text-green-600">${price}</span>
+          <div className="flex flex-row justify-between max-w-fit items-center">
+            <span className="text-lg font-bold text-green-600">${price}</span>
+            <Link>
+              <button className="w-20 p-2 text-sm ml-10 bg-orange text-white">Add Item</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
