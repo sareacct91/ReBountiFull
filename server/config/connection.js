641c3261690fffde2792d1ea2168d9140de83b1b
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-04-16 22:39:15 -0700

    added one more env for mongodb connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ReBountiFull');
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    process.env.MONGODB_LOCAL ||
+    "mongodb://127.0.0.1:27017/ReBountiFull"
+);
 
 module.exports = mongoose.connection;
