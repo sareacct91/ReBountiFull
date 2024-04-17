f2964bce5312895a07389cfc9adca360f791ff51
Sarah Chambers <chamberssarahann@gmail.com>
2024-04-17 16:13:47 -0700

    changed button hovers and added padding to signup

--- client/src/components/AboutUS/aboutus.jsx
+++ client/src/components/AboutUS/aboutus.jsx
@@ -2,8 +2,8 @@ import ImpactImage from "../../assets/images/Aboutus_impact.png"
 
 export default function AboutUs() {
   return(
-    <div className="bg-white">
+    <div className="w-full bg-white">
     <h1 className="text-black text-4xl pt-10 ml-10">Our Impact</h1>
-    <img className="m-auto pb-10" src={ ImpactImage } alt= "Pie chart depicting allocation of food" />
+    <img className="w-full m-auto pb-10" src={ ImpactImage } alt= "Pie chart depicting allocation of food" />
   </div>)
 }
