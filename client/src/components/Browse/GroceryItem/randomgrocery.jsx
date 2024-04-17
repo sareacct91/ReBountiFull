fccaa5bf1d0f5d53e2af790fc21d543d9617df85
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-04-17 04:38:08 -0700

    using useMemo, rendering the search result

--- client/src/components/Browse/GroceryItem/randomgrocery.jsx
+++ client/src/components/Browse/GroceryItem/randomgrocery.jsx
@@ -13,13 +13,6 @@ export default function RandomProducts() {
 
   return (
     <div className="bg-white p-10">
-      <div className="mx-2">
-        <p className=" text-4xl text-black">Browse products</p>
-        <input
-          className="h-10 w-full rounded-2xl border border-zinc-800 bg-white p-5"
-          placeholder="search products..."
-        ></input>
-      </div>
       <div className="grid items-center justify-center bg-white">
         <div className="grid w-fit place-items-center sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
           {Object.values(randomFoodItems).map((food) => (
--- client/src/components/Browse/browse.jsx
+++ client/src/components/Browse/browse.jsx
@@ -1,15 +1,57 @@
+// import { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
-import { useState, useEffect } from "react";
-import { QUERY_FOOD } from "../../utils/queries";
+import { useMemo, useState } from "react";
+import {  QUERY_ALL_FOOD } from "../../utils/queries";
+import GroceryItem from "./GroceryItem/grocery.jsx";
 import RandomProducts from "./GroceryItem/randomgrocery.jsx";
 
-
-
 export default function Browse() {
-    
-    return (
-      <RandomProducts>
-        </RandomProducts>
-    );
+  const { loading, data } = useQuery(QUERY_ALL_FOOD);
+  const [query, setQuery] = useState("");
 
+const filteredItems = useMemo(() => {
+  // Handle case where data is not yet available and the query is not entered
+  if (!data) return [];
+  if (!query) return [];
+  return data.getAllFood.filter((item) => {
+    return item.name.toLowerCase().includes(query.toLowerCase());
+  });
+}, [data, query]);
+
+console.log("filteredItems: ",filteredItems);
+filteredItems.map((i) => console.log(i.name));
+
+// when loading, return loading message and the random items 
+if (loading) return (
+  <>
+    <p>Loading...</p>
+    <RandomProducts></RandomProducts>
+  </>
+);
+  
+  return (
+    <div className="bg-white p-10">
+      <div className="mx-2">
+        <p className="text-4xl text-black">Browse products</p>
+        <input
+          className="h-10 w-full rounded-2xl border border-zinc-800 bg-white p-5 text-black"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          type="search"
+        />
+      </div>
+      <div className="grow-1 flex flex-row flex-wrap text-black">
+        {filteredItems.map(({ _id, name, image, inventory, price }) => (
+          <GroceryItem
+            key={_id}
+            name={name}
+            image={image}
+            inventory={inventory}
+            price={price}
+          />
+        ))}
+      </div>
+      {!query && <RandomProducts />}
+    </div>
+  );
 }
