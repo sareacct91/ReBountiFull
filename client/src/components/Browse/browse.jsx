cf42e4569f9208bf09cf959032b2f9ffbffebd04
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-04-17 17:58:53 -0700

    adding no items message

--- client/src/components/Browse/browse.jsx
+++ client/src/components/Browse/browse.jsx
@@ -1,7 +1,7 @@
 // import { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { useMemo, useState } from "react";
-import {  QUERY_ALL_FOOD } from "../../utils/queries";
+import { QUERY_ALL_FOOD } from "../../utils/queries";
 import GroceryItem from "./GroceryItem/grocery.jsx";
 import RandomProducts from "./GroceryItem/randomgrocery.jsx";
 import Category from "./Category/category.jsx";
@@ -10,26 +10,51 @@ export default function Browse() {
   const { loading, data } = useQuery(QUERY_ALL_FOOD);
   const [query, setQuery] = useState("");
 
-const filteredItems = useMemo(() => {
-  // Handle case where data is not yet available and the query is not entered
-  if (!data) return [];
-  if (!query) return [];
-  return data.getAllFood.filter((item) => {
-    return item.name.toLowerCase().includes(query.toLowerCase());
-  });
-}, [data, query]);
+  const filteredItems = useMemo(() => {
+    // Handle case where data is not yet available and the query is not entered
+    if (!data) return [];
+    if (!query) return [];
+    return data.getAllFood.filter((item) => {
+      return item.name.toLowerCase().includes(query.toLowerCase());
+    });
+  }, [data, query]);
 
-console.log("filteredItems: ",filteredItems);
-filteredItems.map((i) => console.log(i.name));
+  console.log("filteredItems: ", filteredItems);
+  filteredItems.map((i) => console.log(i.name));
 
-// when loading, return loading message and the random items 
-if (loading) return (
-  <>
-    <p>Loading...</p>
-    <RandomProducts></RandomProducts>
-  </>
-);
+  // when loading, return loading message and the random items
+  if (loading)
+    return (
+      <>
+        <p>Loading...</p>
+        <RandomProducts></RandomProducts>
+      </>
+    );
   
+    // if there is no item matching, render the message : "No Itmes found"
+  if (query && filteredItems.length === 0) {
+    return (
+      <div className="bg-white p-10">
+        <div className="mx-2 grid w-full">
+          <p className="text-4xl text-black">Browse products</p>
+          <input
+            className="mt-6 h-10 w-full rounded-2xl border border-zinc-800 bg-white p-5 text-black"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="search products..."
+            type="search"
+          />
+        </div>
+        <div className="bg-white p-10">
+          <div className="mx-2 grid w-full">
+            <p className="text-4xl text-black">No items found</p>
+          </div>
+          <Category />
+          <RandomProducts />
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="bg-white p-10">
       <div className="mx-2 grid w-full">
