66f3e3298e7f71128ce35992d0b2acde4e7b5ff9
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-04-18 00:45:35 -0700

    added the preference name dynamically

--- client/src/components/Browse/Preference/preference.jsx
+++ client/src/components/Browse/Preference/preference.jsx
@@ -1,19 +1,32 @@
-import * as preferences from "../../../assets/preference/index.js"
+import * as preferences from "../../../assets/preference/index.js";
 import { useQuery } from "@apollo/client";
 import { useState } from "react";
 import { QUERY_FOOD_BY_PREFERENCE } from "../../../utils/queries.js";
 import GroceryItem from "../GroceryItem/grocery.jsx";
 
 const preferenceData = [
-    { name: "dairyFree", source: preferences.dairyFree, value: { dairyFree: true } },
-    { name: "glutenFree", source: preferences.glutenFree, value: { glutenFree: true } },
-    { name: "nutFree", source: preferences.nutFree, value: { nutFree: true } },
-    { name: "vegan", source: preferences.vegan, value: { vegan: true } },
-    { name: "vegetarian", source: preferences.vegetarian, value: { vegetarian: true } },
-  ];
-
+  {
+    name: "dairyFree",
+    source: preferences.dairyFree,
+    value: { dairyFree: true },
+  },
+  {
+    name: "glutenFree",
+    source: preferences.glutenFree,
+    value: { glutenFree: true },
+  },
+  { name: "nutFree", source: preferences.nutFree, value: { nutFree: true } },
+  { name: "vegan", source: preferences.vegan, value: { vegan: true } },
+  {
+    name: "vegetarian",
+    source: preferences.vegetarian,
+    value: { vegetarian: true },
+  },
+];
 
 export default function Preference() {
+  const [selectedPreferenceName, setSelectedPreferenceName] = useState("");
+
   // default value is false for every preference field
   const [selectedPreference, setSelectedPreference] = useState({
     vegan: false,
@@ -27,17 +40,18 @@ export default function Preference() {
     variables: selectedPreference,
   });
 
-
   if (loading) {
     return <p>Loading...</p>;
   }
 
-  const handleQuery = (preference) => {
-    setSelectedPreference(preference);
+  const handleQuery = (preferenceName) => {
+    const preference = preferenceData.find((p) => p.name === preferenceName);
+    setSelectedPreference(preference.value);
+    setSelectedPreferenceName(preferenceName);
   };
 
   // Filter out items that match the selected preferences
-  const filteredItems = data.getFoodByPreference
+  const filteredItems = data.getFoodByPreference;
 
   return (
     <div>
@@ -45,12 +59,12 @@ export default function Preference() {
         <div className="col-span-full my-6 text-2xl text-black">
           <p>Browse by Preference</p>
         </div>
-        {/* Rendering categories */}
+        {/* Rendering preference */}
         {preferenceData.map((preference, index) => (
           <div
             key={index}
             className="flex flex-col items-center"
-            onClick={() => handleQuery(preference.value)}
+            onClick={() => handleQuery(preference.name)}
           >
             <img
               src={preference.source}
@@ -62,9 +76,10 @@ export default function Preference() {
         ))}
 
         <div className="col-span-full my-6">
-          {/* Render the category name */}
-          <p className="text-2xl text-black"></p>
+          {/* Render the preference name */}
+          <p className="text-2xl text-black">{selectedPreferenceName}</p>
         </div>
+
         <div className="col-span-full mb-10 grid place-items-center border-b-2 border-black pb-20">
           <div className="col-span-full grid w-fit flex-row flex-wrap place-items-center text-black sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
             {/* Render grocery items based on the category*/}
