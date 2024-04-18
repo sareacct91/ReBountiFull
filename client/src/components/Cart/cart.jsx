ff3b0c4b6ad37eab641c8714f135874d8956f38c
MeowRoar <sareacct91@gmail.com>
2024-04-18 01:01:04 -0700

    Update Cart Component
    - refactor: move the item selection into it's own component
    - edit styling
    - Add remove button and it's functionality
    - Add range slider to adjust the payment amount
    - Add element to display the user payment amount

--- client/src/components/Cart/cart.jsx
+++ client/src/components/Cart/cart.jsx
@@ -1,47 +1,86 @@
 import { useMutation, useQuery } from "@apollo/client";
-import { useParams } from "react-router-dom";
 import { QUERY_CART } from "../../utils/queries";
 import ShoppingBagImg from "../../assets/images/shopping_bag.png";
+import CartItem from "./cartItem";
+import { useState } from "react";
 
 export default function Cart() {
-  const { userId } = useParams();
   const { loading, error, data } = useQuery(QUERY_CART);
+  const [rangeValue, setRangeValue] = useState(0);
 
   if (loading) return <p>Loading...</p>;
+  if (error) {
+    alert(error);
+  }
 
   const { getCart } = data;
-  console.log(getCart);
+  // console.log(getCart);
+  console.log(rangeValue)
 
-  const testList = getCart.items.map((e) => (
-    <div key={e.id} className="m-2 grid-cols-4">
-      <div className="flex flex-row justify-between">
-        <img src={e.images[0]} alt={e.name} className="rounded-xl" />
-        <div className="flex flex-col items-center justify-center w-1/2">
-          <p className="ml-20 ">{e.name}</p>
-          <p className="ml-20 w-full flex items-start justify-evenly"><span className="">quantity</span>{e.quantity}</p>
-          <p className="ml-20 w-full flex items-start justify-evenly"><span>each</span>{e.unitTotal.formatted}</p>
-          <p className="ml-20 w-full flex items-start justify-evenly"><span>total</span>{e.lineTotal.formatted}</p>
-        </div>
-      </div>
-    </div>
-  ));
 
   return (
     <>
-      <div className="grid w-full place-items-center">
+      <div className="grid w-full place-items-center bg-white text-black">
+        <h1 className="flex flex-row">
+          My Cart
+          <img src={ShoppingBagImg} alt="Shopping bag icon" />
+        </h1>
+        <div className="w-5/6 rounded-xl border border-black lg:w-1/2 lg:p-10">
+          <div className="flex w-full flex-col gap-2">
+            {getCart.items.map((item) => (
+              <CartItem key={item.id} item={item} />
+            ))}
+          </div>
+          <div className="m-auto w-1/2 p-5 lg:w-3/4">
+            <p className="flex w-full justify-between">
+              <span className="underline">item(s) total:</span>
+              {getCart.totalItems} items
+            </p>
+            <p className="flex w-full justify-between">
+              <span className="underline">Grand Total:</span>
+              {getCart.grandTotal.formatted}
+            </p>
+            <p className="flex w-full justify-between">
+              <span className="underline">What you pay:</span>
+              ${(rangeValue / 100).toFixed(2)}
+            </p>
+            <input className="w-full " 
+              type="range" 
+              min="0"
+              max={getCart.grandTotal.amount}
+              onChange={e => setRangeValue(e.target.value)} 
+              defaultValue={getCart.grandTotal.amount}
+              value={rangeValue}
+            />
+          </div>
+         <div className="flex w-full justify-center lg:justify-end">
+            <button className="bg-blue-600 text-white">check out</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+
+  return (
+    <>
+      <div className="grid w-full place-items-center bg-white text-black">
         <h1 className="flex flex-row">
           My Cart
           <img src={ShoppingBagImg} alt="Shopping bag icon" />
         </h1>
         <div className="grid h-5/6 w-1/2 place-items-center rounded-xl border border-black p-10">
-          <div className="w-full ">{testList}</div>
-          <div className="w-full">
-            <p className="w-full flex justify-evenly">
-              <span>item(s) total:</span>
+          <div className="flex w-full flex-col gap-2">
+            {getCart.items.map((item) => (
+              <CartItem key={item.id} item={item} />
+            ))}
+          </div>
+          <div className="w-full p-5">
+            <p className="flex w-full justify-evenly">
+              <span className="underline">item(s) total:</span>
               {getCart.totalItems} items
             </p>
-            <p className="w-full flex justify-evenly">
-              <span>Grand Total:</span>
+            <p className="flex w-full justify-evenly">
+              <span className="underline">Grand Total:</span>
               {getCart.grandTotal.formatted}
             </p>
           </div>
---
+++ client/src/components/Cart/cartItem.jsx
@@ -0,0 +1,88 @@
+import { useMutation } from "@apollo/client"
+import { REMOVE_CART_ITEM } from "../../utils/mutations"
+import PropTypes from "prop-types";
+
+export default function CartItem({item}) {
+  const [removeCartItem] = useMutation(REMOVE_CART_ITEM);
+
+  async function clickedRemoveItem() {
+    try {
+      console.log(item.id)
+      const {data: removeItem} = await removeCartItem({ 
+        variables: { 
+          food: {
+            _id: item.id
+          }
+        }
+      }); 
+
+    } catch (err) {
+      console.error(err); 
+    }
+  }
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-between border-b border-black p-5 lg:flex-row lg:rounded-xl lg:border">
+        <img src={item.images[0]} alt={item.name} className="rounded-xl" />
+        <div className="flex w-3/4 flex-col items-center sm:justify-center lg:w-1/2">
+          <p>{item.name}</p>
+          <p className="min-w-60 lg:min-w-40 flex w-1/2 justify-between lg:w-full lg:items-start">
+            <span>quantity</span>
+            {item.quantity}
+          </p>
+          <p className="min-w-60 lg:min-w-40 flex w-1/2 justify-between lg:w-full lg:items-start">
+            <span>each</span>
+            {item.unitTotal.formatted}
+          </p>
+          <p className="min-w-60 lg:min-w-40 flex w-1/2 justify-between lg:w-full lg:items-start">
+            <span>total</span>
+            {item.lineTotal.formatted}
+          </p>
+          <button 
+            className="self-end text-blue-400 underline" 
+            onClick={clickedRemoveItem}>
+            remove
+          </button> 
+        </div>
+      </div>
+    </>
+  )
+}
+
+CartItem.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    unitTotal: PropTypes.shape({
+      formatted: PropTypes.string.isRequired,
+      amount: PropTypes.number
+    }).isRequired,
+    lineTotal: PropTypes.shape({
+      formatted: PropTypes.string.isRequired,
+      amount: PropTypes.number
+    }).isRequired
+  }).isRequired
+};
+
+
+
+// export default function CartItem({item}) {
+//   return (
+//     <>
+//       <div className="m-2 grid-cols-4">
+//         <div className="flex flex-row justify-between">
+//           <img src={item.images[0]} alt={item.name} className="rounded-xl" />
+//           <div className="flex w-1/2 flex-col items-center justify-center">
+//             <p className="ml-20 ">{item.name}</p>
+//             <p className="ml-20 flex w-full items-start justify-evenly"><span className="">quantity</span>{item.quantity}</p>
+//             <p className="ml-20 flex w-full items-start justify-evenly"><span>each</span>{item.unitTotal.formatted}</p>
+//             <p className="ml-20 flex w-full items-start justify-evenly"><span>total</span>{item.lineTotal.formatted}</p>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
