f423fd3d1a70cce6c963d53b13264aa89f13615d
MeowRoar <sareacct91@gmail.com>
2024-04-19 17:02:57 -0700

    Add CART_CHECKOUT query
    - remove abandoned from GET_CART

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -108,7 +108,6 @@ export const QUERY_CART = gql`
       id
       totalItems
       totalUniqueItems
-      abandoned
       grandTotal {
         amount
         formatted
@@ -127,6 +126,14 @@ export const QUERY_CART = gql`
         }
         quantity
       }
+    }
+  }
+`;
+
+export const CART_CHECKOUT = gql`
+  query CartCheckout($order: OrderInput!) {
+    cartCheckout(order: $order) {
+      session
     }
   }
 `;
