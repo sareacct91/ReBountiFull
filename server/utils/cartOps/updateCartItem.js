9846db8025522318739b1acdd1299199e69cf62f
MeowRoar <sareacct91@gmail.com>
2024-04-21 07:39:57 -0700

    add: method to update Cart

---
+++ server/utils/cartOps/updateCartItem.js
@@ -0,0 +1,33 @@
+const { Cart } = require("../../model");
+const shapeCartData = require("./shapeCartData");
+
+async function updateCartItem({ food, id }) {
+  // console.log('\ncartOps updateCartItem: \n');
+  try {
+    let cart = await Cart.findOneAndUpdate(
+      { id, 'items.id': food._id },
+      { $set: { 'items.$.quantity': food.quantity }},
+      { 
+        new: true, 
+        runValidators: true, 
+        projection: '-_id',
+        populate: {
+          path: 'items.id',
+          select: '_id name price inventory image'
+        } 
+      }
+    );
+
+    if (!cart) {
+      throw new Error("failed to update cart");
+    }
+
+    return shapeCartData(cart.toObject());
+  } catch (err) {
+    console.error(err);
+    return { error: err };
+  } 
+}
+
+
+module.exports = updateCartItem;
