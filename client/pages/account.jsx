654146bde05770997ac7402a2e4c41beb2cceeed
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-04-22 14:52:42 -0700

    initial commit for accont

--- client/pages/account.jsx
+++ client/pages/account.jsx
@@ -16,21 +16,16 @@ export default function Account() {
   console.log(data);
 
   return (
-    <div className="flex min-h-[calc(100vh-192px-40px)] w-full flex-col items-center gap-5 bg-white text-black">
-      <div className="mt-5 rounded-lg border-2 border-gray-500 px-16 py-12">
+    <div className="flex min-h-[calc(100vh-192px-40px)] w-full flex-col gap-5 bg-white text-black">
+      <div className="mt-5">
         <h1 className="pb-10 text-center">{} Account</h1>
         <div>
           <p></p>
         </div>
       </div>
 
-      <div className="mx-2 mb-2 flex max-w-xl flex-col rounded-lg border-8 border-double border-blue-500 bg-blue-300/40 p-5 font-semibold text-black">
-        <p className="">
-          ReBountiFull helpled my family when we didn't know where our next meal
-          was coming from. It made a world of difference for my growing
-          children.
-          <br />- Mary, a single mom from San Jose
-        </p>
+      <div className="">
+        <h2>Purchase History</h2>
       </div>
     </div>
   )
