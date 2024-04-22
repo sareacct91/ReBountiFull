00a7271f58a8d67bb20b4e5c83c122bf1b497657
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-04-22 16:44:38 -0700

    account page done

--- client/pages/account.jsx
+++ client/pages/account.jsx
@@ -1,11 +1,12 @@
 import { useQuery } from "@apollo/client";
 import Auth from "../src/utils/auth"
 import { QUERY_USER } from "../src/utils/queries";
+import History from '../src/components/History/history';
 
 export default function Account() {
   const { data, loading: loadingUser, error: errorUser } = useQuery(QUERY_USER);
 
-  if (loadingUser || errorUser ) {
+  if (loadingUser || errorUser) {
     console.log('loading', loadingUser);
     console.log('error', errorUser)
     return (
@@ -13,19 +14,44 @@ export default function Account() {
     )
   }
 
-  console.log(data);
+  const { user } = data;
+  const {
+    business_name,
+    email,
+    fullname,
+    history,
+    household_size,
+    isClient,
+    isSupplier,
+    username,
+  } = user;
+
+  console.log("HISTORY", history);
 
   return (
-    <div className="flex min-h-[calc(100vh-192px-40px)] w-full flex-col gap-5 bg-white text-black">
-      <div className="mt-5">
-        <h1 className="pb-10 text-center">{} Account</h1>
-        <div>
-          <p></p>
+    <div className="flex flex-col min-h-[calc(100vh-192px-40px)] bg-white text-black gap-5">
+      <div className="mt-5 flex flex-col">
+        <h1 className="pb-10 text-center">{fullname}'s Account</h1>
+        <div className="mx-auto w-1/2 flex flex-col h-max gap-2">
+          <p className="w-max"><span className="font-bold">Username:</span> {username}</p>
+          <p className="w-max"><span className="font-bold">Email:</span> {email}</p>
+          { isClient && <p className="w-max"><span className="font-bold">Household Size: </span>{household_size}</p> }
+          { isSupplier && <p className="w-max"><span className="font-bold">Business Name: </span>{business_name}</p> }
         </div>
       </div>
 
-      <div className="">
-        <h2>Purchase History</h2>
+      <div className="flex flex-col mx-auto w-1/2">
+        <h3 className="text-lg font-bold">Purchase History:</h3>
+        <div>
+          <ul>
+            {history.map((e) => {
+              return (
+                <History key={e.stripeId} {...e} />
+              )
+            }
+            )}
+          </ul>
+        </div>
       </div>
     </div>
   )
